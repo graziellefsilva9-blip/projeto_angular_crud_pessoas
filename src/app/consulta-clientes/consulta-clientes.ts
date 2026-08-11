8dc02af2b597f5ee1403b5891a9c69945b9d2c6b
Tarde Cetafaju <Tarde.Cetafaju@labfies.org.br>
2026-08-11 16:37:24 -0300

    criado uma pasta consulta

---
+++ src/app/consulta-clientes/consulta-clientes.ts
@@ -0,0 +1,42 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ClienteService } from '../../services/cliente.service';
+import { Cliente } from '../../models/cliente';
+
+@Component({
+  selector: 'app-consulta-clientes',
+  standalone: true,
+  imports: [
+    CommonModule
+  ],
+  templateUrl: './consulta-clientes.html',
+  styleUrl: './consulta-clientes.css'
+})
+export class ConsultaClientes {
+
+  clientes: Cliente[] = [];
+
+  constructor(
+    private clienteService: ClienteService
+  ) {
+
+    this.atualizarLista();
+
+  }
+
+  atualizarLista() {
+
+    this.clientes = this.clienteService.listar();
+
+  }
+
+  excluir(id: number) {
+
+    this.clienteService.excluir(id);
+
+    this.atualizarLista();
+
+  }
+
+}
